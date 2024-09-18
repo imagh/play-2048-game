0be69ccc729c7e5fe572284d0f60463054acceb1
Akash G <akashg.sde@gmail.com>
2024-09-18 21:22:46 +0530

    enhancement: server: setup CORS
    Setup CORS to listen to all IPs.
    
    Signed-off-by: Akash G <akashg.sde@gmail.com>

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  return res.send("Server's up!");
+app.use(cors({
+  origin: true
+}));
+
+app.listen(process.env.PORT, "0.0.0.0", () => {
+  console.log(`Server is listening on ${process.env.PORT}`);
 });
 
-app.listen(process.env.PORT);
-console.log(`Server is listening on ${process.env.PORT}`);
+app.use('/', (req, res, next) => {
+  return res.send((parseInt(Math.random() * 10) % 4).toString());
+});
